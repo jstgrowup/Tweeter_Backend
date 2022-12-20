15d3b76033605fcb4832c1b079cff824431826cf
jstgrowup <deysubham999@gmail.com>
2022-12-20 22:45:17 +0530

    TimeLine todo

---
+++ src/Routes/User.route.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const userModel = require("../Models/User.model");
+const app = express.Router();
+app.post("/postUser", async (req, res) => {
+  //   const { email } = req.body;
+  //   console.log('req:', req.body)
+  //   const data = await userModel.findOne({ email: email });
+
+  try {
+    // if (data) {
+    //   res.status(401).send("User already exists");
+    // } else {
+    const huru = await userModel.create(req.body);
+    res.send(huru);
+    // }
+  } catch (error) {
+    res.status(401).send(error.message);
+  }
+});
+app.post("/login", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const data = await userModel.findOne({
+    email: email,
+    username: username,
+    password: password,
+  });
+
+  try {
+    if (!data.username) {
+      res.status(401).send("User not Signed up");
+    } else {
+      res.send(data);
+    }
+  } catch (error) {
+    res.status(401).send(error.message);
+  }
+});
+app.post("/getuser", async (req, res) => {
+  const { id } = req.body;
+  console.log("id:", id);
+
+  try {
+    const respo = await userModel.findOne({ _id: id });
+    res.send(respo);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+app.patch("/updateUser/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const { fullname, email, password } = req.body;
+
+  try {
+    const data = await userModel.findByIdAndUpdate(
+      { _id: id },
+      { $set: { fullname: fullname, email: email, password: password } }
+    );
+    console.log("data:", data);
+
+    res.send(data);
+  } catch (error) {
+    res.status(401).send(error.message);
+  }
+});
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const userRouter = require("./Routes/User.route");
 require("dotenv").config();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 8080;
 const MONGOURL = process.env.MONGO_URL;
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
 mongoose.set("strictQuery", false);
-app.use(cors());
 app.get("/", (req, res) => {
   res.send("HOMEPAGE");
 });
+app.use("/user", userRouter);
 app.listen(PORT, () => {
   mongoose
     .connect(MONGOURL)
